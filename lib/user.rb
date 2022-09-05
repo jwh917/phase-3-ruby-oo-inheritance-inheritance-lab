d3ec7bf547aec9e205d914bdd2904cc400b4ecf8
Julien Hnery <jwh_917@yahoo.com>
2022-09-05 18:35:14 -0400

    Object Inheritance

---
+++ lib/user.rb
@@ -0,0 +1,18 @@
+class User
+
+  attr_accessor :first_name, :last_name
+
+  # def initialize(first_name, last_name)
+  #   @first_name = first_name
+  #   @last_name = last_name
+  # end
+
+  # def go
+  #   "vrrrrrrrooom!"
+  # end
+
+  # def fill_up_tank
+  #   "filling up!"
+  # end
+
+end
